35b05e63e805778ea5ef7be3c423eb086e005107
gilzoide <gilzoide@gmail.com>
2023-10-12 12:16:02 -0300

    Add unit test for GDObject handling notifications

--- test/unit_tests/GDObject.gd
+++ test/unit_tests/GDObject.gd
@@ -1,26 +1,45 @@
 extends RefCounted
 
 static var ObjectiveC = Engine.get_singleton("ObjectiveC")
+const NOTIFICATION_NAME = "my cool notification"
+const NOTIFICATION_USER_INFO = { hello = "world" }
 
 var my_property = 5
+var notification_called = false
 
 
 func test_property() -> bool:
-	var objcObject = ObjectiveC.wrap_object(self)
-	assert(objcObject.my_property == my_property)
-	assert(objcObject.invoke("my_property") == my_property)
+	var self_objc = ObjectiveC.wrap_object(self)
+	assert(self_objc.my_property == my_property)
+	assert(self_objc.invoke("my_property") == my_property)
 	return true
 
 
 func test_method() -> bool:
-	var objcObject = ObjectiveC.wrap_object(self)
-	assert(objcObject.invoke("intToBool:", 1) == true)
-	assert(objcObject.invoke("intToBool:", 0) == false)
+	var self_objc = ObjectiveC.wrap_object(self)
+	assert(self_objc.invoke("intToBool:", 1) == true)
+	assert(self_objc.invoke("intToBool:", 0) == false)
+	return true
+
+
+func test_notification() -> bool:
+	var self_objc = ObjectiveC.wrap_object(self)
+	var NSNotificationCenter = ObjectiveC.NSNotificationCenter.defaultCenter
+	NSNotificationCenter.invoke("addObserver:selector:name:object:", self_objc, "handleNotification:", NOTIFICATION_NAME, null)
+	NSNotificationCenter.invoke("postNotificationName:object:userInfo:", NOTIFICATION_NAME, null, NOTIFICATION_USER_INFO)
+	NSNotificationCenter.invoke("removeObserver:", self_objc)
+	assert(notification_called, "Notification was not called")
 	return true
 
 
 func intToBool(value: int) -> bool:
 	return bool(value)
+
+
+func handleNotification(notification):
+	assert(notification.name == NOTIFICATION_NAME)
+	assert(notification.userInfo.to_dictionary() == NOTIFICATION_USER_INFO)
+	notification_called = true
 
 
 func methodSignatureForSelector(sel: String) -> String:
@@ -29,5 +48,7 @@ func methodSignatureForSelector(sel: String) -> String:
 			return "i@:"
 		"intToBool:":
 			return "B@:i"
+		"handleNotification:":
+			return "v@:@"
 		_:
 			return ""
